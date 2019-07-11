20609fe64f88de293210db82a4b519c37af4dc3e
tkow <t.kusama0613@outlook.jp>
2019-07-12 00:22:57 +0900

    とりあえず一番近い形になった

--- src/firebase-definitions.ts
+++ src/firebase-definitions.ts
@@ -8,37 +8,61 @@ interface PathInitializerBase {
 
 type PathInitializer = OptionalInitializerParams & PathInitializerBase
 
-type DDD<O> = {
-  [key in keyof O]?: PathInitializer
-}
-
-type Z<ZX> = DDD<ZX> & OptionalInitializerParams
-
-type C<D> = {
-  [s in keyof D]: PathInitializer
-}
-
-type F = C<PathInitializer>
-
 const a = {
   path: {
-    subpath: {}
+    subpath: {
+      s: {}
+    }
   }
 }
 
-Object.keys(a).reduce((current, key) => {
-  return
-}, {})
-
-type c = {
-  path: <A>(
-    path: keyof A
-  ) => {
-    subPath: <B>(path: keyof B) => string
-  }
+interface Fu<O extends PathInitializer> {
+  <T extends string>(path?: T): typeof path extends undefined ? string : ReturnType<Fa<O>>
 }
 
-const b = new A({ a: {} })
+interface Fa<O extends PathInitializer> {
+  (o: O, _path: string): O extends object ? { [s in keyof O]: Fu<O[keyof O]> } : string
+}
+
+function fa<O extends PathInitializer>(o: O, _path: string): ReturnType<Fa<O>> {
+  const d = Object.keys(o) as (keyof O)[]
+  if (d.length) {
+    return d.reduce((a, b, c) => {
+      const func = (id?: string) => (id ? fa(o[b], `${_path}/${b}/${id}`) : `${_path}/${b}`)
+      func.getPath = () => _path
+      return {
+        ...a,
+        [b]: func
+      }
+    }, {}) as ReturnType<Fa<O>>
+  }
+  return 'test' as ReturnType<Fa<O>>
+}
+
+const z = fa(a, '')
+const d = z
+  .path()
+  .subpath()
+  .s()
+d.ffa = 'test'
+
+// const dd = Object.keys(a).reduce((current,_key)=>{
+//   // tslint:disable-next-line:no-unused-expression
+//   const fafa = Object.keys(a)
+//   a[fafa[0]]
+//   return {
+//     ...current,
+//     [_key]:(_a?:string) => {
+//       const result = _key+_a!
+//       if(!( a[_key])) return
+//       return Object.keys(a[key]).reduce((current,key)=>{
+//         return {
+//           [key]: 'test'
+//         }
+//       },{})
+//     }
+//   }
+// },{})
 
 //   const collections = {
 //     'users': {
